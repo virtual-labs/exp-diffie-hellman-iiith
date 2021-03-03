f5cd0acf5b2a428f607ba523c735b3ca16c35c3b
sravanthimodepu <sravanthimodepu@gmail.com>
2021-03-03 16:13:47 +0530

    added quiz and simulation

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -89,25 +89,54 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. 101 <sub>4,800,000,023</sub> mod 35.", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "36", ///// Write the option 1 inside double quotes
+                b: "9", ///// Write the option 2 inside double quotes
+		c: "26", ///// Write the option 1 inside double quotes
+                d: "17", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detectorWhich among these is a generator of Z*13",  ///// Write the question inside double quotes
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
+        a: "3", ///// Write the option 1 inside double quotes
+        b: "4", ///// Write the option 2 inside double quotes
+        c: "7",
+	d: "2",
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+
+
+    {
+      question: "The set of quadratic residues modulo 211 has cardinality of",  ///// Write the question inside double quotes
+      answers: {
+        a: "210", ///// Write the option 1 inside double quotes
+        b: "106", ///// Write the option 2 inside double quotes
+        c: "212",
+	d: "105",
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+
+
+    {
+      question: "4. N=90 then Ø(n) =? Where Ø(n) is the number of elements co-prime to ‘n’ [Ø(n) is also called Euler Totient function]",  ///// Write the question inside double quotes
+      answers: {
+        a: "8", ///// Write the option 1 inside double quotes
+        b: "24", ///// Write the option 2 inside double quotes
+        c: "48",
+	d: "14",
+      },
       correctAnswer: "c"                ///// Write the correct option inside double quotes
     },
 
 
 
+			  
      ];
 
 
